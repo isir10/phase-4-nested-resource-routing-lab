be60a9ef1e689663a0c13fa9348d40dcfd9946ea
isir10 <isir.malak@moringaschool.com>
2022-12-04 21:52:37 +0300

    commit tree 917124b6e6f6c5e0b42d957141ecff80c05c489a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
